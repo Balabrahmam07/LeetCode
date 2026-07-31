b67a6839dbc8514b0100ff86e26e4958933519c7
Balabrahmam07 <136158779+Balabrahmam07@users.noreply.github.com>
2026-07-31 15:48:44 +0530

    Time: 92 ms (85.95%), Space: 20 MB (48.16%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,12 +1,12 @@
 from collections import Counter
+
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        word = Counter(word)
+        freq = Counter(word)
+
         count = 0
-        alpha_list = [[] for _ in range(8)]
-        for i, (wrd, freq) in enumerate(word.most_common()):
-            idx = i % 8
-            if len(alpha_list[idx]) < 8:
-                alpha_list[idx].append(wrd)
-                count += (alpha_list[idx].index(wrd) + 1) * freq
+
+        for i, (_, f) in enumerate(freq.most_common()):
+            count += (i // 8 + 1) * f
+
         return count
